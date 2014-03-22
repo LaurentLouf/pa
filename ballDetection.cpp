7e5748e9b8d9aa0f1d823f172bd1fde232e1ecb0
Manchel <simon_manchel@hotmail.fr>
2014-03-22 21:20:59 +0100

    Implementation du filtrage 100% ocl + modifs mineures. Sur 7319.MOV : 12.15s environ

--- ballDetection.cpp
+++ ballDetection.cpp
@@ -10,6 +10,7 @@
 #include <ctime>
 #include <sstream>
 #include <cmath>
+#include <sys/time.h>
 
 //#define DISPLAY
 #define HOUGHh
@@ -29,11 +30,14 @@ int S_max = 255 ;
 int V_min = 0 ;
 int V_max = 255 ;
 
-static const char WINDOW_ORIGIN[] = "Original Image";
-static const char WINDOW_THRESHOLD[] = "Image with threshold applied";
-static const char WINDOW_THRESHOLD_NOISE[] = "Image with threshold applied and noise cancelation";
-static const char WINDOW_THRESHOLD_NOISE_BLUR[] = "Image with threshold applied and noise cancelation and blur";
-static const char WINDOW_CONFIG[] = "Configuration";
+#ifdef DISPLAY
+    static const char WINDOW_ORIGIN[] = "Original Image";
+    static const char WINDOW_THRESHOLD[] = "Image with threshold applied";
+    static const char WINDOW_THRESHOLD_NOISE[] = "Image with threshold applied and noise cancelation";
+    static const char WINDOW_THRESHOLD_NOISE_BLUR[] = "Image with threshold applied and noise cancelation and blur";
+    static const char WINDOW_CONFIG[] = "Configuration";
+    static const char WINDOW_TEST[] = "Test";
+#endif
 
 static const char WINDOW_HUE[] = "Channel Hue";
 static const char WINDOW_LIGHT[] = "Channel Lightness";
@@ -65,6 +69,10 @@ static void onMouse( int event, int x, int y, int /*flags*/, void* channel )
 
 int main( int argc, char** argv )
 {
+    // Benchmark variables
+    struct timeval tv_begin, tv_end, beginTime, endTime;
+    double filteringTime=0 , noiseTime=0, contourTime=0, readTime=0, totalTime; 
+
     // Image processing vars
     Mat image, imageFiltered, imageTmp ;
     vector<Vec3f> circles;
@@ -84,7 +92,7 @@ int main( int argc, char** argv )
     Size subPixWinSize(10,10), winSize(31,31);
 
     // Display vars
-    float begin, end, beginFiltering, endFiltering, filteringTime = 0, beginNoise, endNoise, noiseTime = 0, beginContour, endContour, contourTime = 0, calculationTime = 0, worst = 0, best = 100000000000000     ;
+    //double begin, end, beginFiltering, endFiltering, filteringTime = 0, beginNoise, endNoise, noiseTime = 0, beginContour, endContour, contourTime = 0, calculationTime = 0, worst = 0, best = 100000000000000     ;
     int i = 0 ;
     stringstream stringOutput;
 
@@ -97,86 +105,100 @@ int main( int argc, char** argv )
         return 0;
     }
 
-    // Create the windows
-    namedWindow(WINDOW_ORIGIN) ;
-    namedWindow(WINDOW_THRESHOLD);
-    namedWindow(WINDOW_THRESHOLD_NOISE);
-    namedWindow(WINDOW_THRESHOLD_NOISE_BLUR);  
-    namedWindow(WINDOW_CONFIG);  
+    #ifdef DISPLAY
+        // Create the windows
+        namedWindow(WINDOW_ORIGIN) ;
+        namedWindow(WINDOW_THRESHOLD);
+        namedWindow(WINDOW_THRESHOLD_NOISE);
+        namedWindow(WINDOW_THRESHOLD_NOISE_BLUR);  
+        namedWindow(WINDOW_CONFIG);  
 
-    createTrackbar(TRACKBAR_HUE_MIN, WINDOW_CONFIG, &H_min, 255) ;  
-    createTrackbar(TRACKBAR_HUE_MAX, WINDOW_CONFIG, &H_max, 255) ;  
-    createTrackbar(TRACKBAR_SATURATION_MIN, WINDOW_CONFIG, &S_min, 255) ;  
-    createTrackbar(TRACKBAR_SATURATION_MAX, WINDOW_CONFIG, &S_max, 255) ;  
-    createTrackbar(TRACKBAR_VALUE_MIN, WINDOW_CONFIG, &V_min, 255) ;  
-    createTrackbar(TRACKBAR_VALUE_MAX, WINDOW_CONFIG, &V_max, 255) ;  
+        createTrackbar(TRACKBAR_HUE_MIN, WINDOW_CONFIG, &H_min, 255) ;  
+        createTrackbar(TRACKBAR_HUE_MAX, WINDOW_CONFIG, &H_max, 255) ;  
+        createTrackbar(TRACKBAR_SATURATION_MIN, WINDOW_CONFIG, &S_min, 255) ;  
+        createTrackbar(TRACKBAR_SATURATION_MAX, WINDOW_CONFIG, &S_max, 255) ;  
+        createTrackbar(TRACKBAR_VALUE_MIN, WINDOW_CONFIG, &V_min, 255) ;  
+        createTrackbar(TRACKBAR_VALUE_MAX, WINDOW_CONFIG, &V_max, 255) ;  
 
-    moveWindow(WINDOW_ORIGIN, 0, 0) ;
-    moveWindow(WINDOW_THRESHOLD, 0, 0);
-    moveWindow(WINDOW_THRESHOLD_NOISE, 0, 0);
-    moveWindow(WINDOW_THRESHOLD_NOISE_BLUR, 0, 0);
-    moveWindow(WINDOW_CONFIG, 0, 0);
+        moveWindow(WINDOW_ORIGIN, 0, 0) ;
+        moveWindow(WINDOW_THRESHOLD, 0, 0);
+        moveWindow(WINDOW_THRESHOLD_NOISE, 0, 0);
+        moveWindow(WINDOW_THRESHOLD_NOISE_BLUR, 0, 0);
+        moveWindow(WINDOW_CONFIG, 0, 0);
+
+        namedWindow(WINDOW_TEST);
+        moveWindow(WINDOW_TEST, 0, 0);
+    #endif
 
     #ifdef OCL
         ocl::PlatformsInfo platforms;
         ocl::getOpenCLPlatforms(platforms);
         ocl::DevicesInfo devices;
         ocl::getOpenCLDevices(devices);
-        std::cout << "platforms " << platforms.size() << "  devices " << devices.size() << std::endl;
+        std::cout << "platforms " << platforms.size() << "  devices " << devices.size() << " " << devices[0]->deviceDriverVersion << std::endl;
         ocl::setDevice(devices[0]);
 
     #endif
 
-    size_t beginTime = time(NULL);
+    gettimeofday(&beginTime, NULL);
 
     for(;;)
     {
-        begin = (float) clock() ;
+        //begin = (float) clock() ;
         i++ ;
 
-
+        gettimeofday(&tv_begin, NULL);
         Mat frame;
         cap >> frame;
         if( frame.empty() )
             break;
 
         frame.copyTo(image);
+        gettimeofday(&tv_end, NULL);
+        readTime += (double) (tv_end.tv_sec - tv_begin.tv_sec) + ((double) (tv_end.tv_usec - tv_begin.tv_usec)/1000000);
 
         // Filtering part : conversion from one color space to another
-        beginFiltering = (float) clock() ;
+        //beginFiltering = (float) clock() ;
+        gettimeofday(&tv_begin, NULL);
         #ifdef OCL
-            ocl::oclMat oclImage(image), oclImageFiltered, oclCircles;
-            ocl::cvtColor(oclImage, oclImageFiltered, CV_RGB2HSV);
+            ocl::oclMat oclImage(image), oclImageFilteredCvt, oclImageFiltered, oclCircles, thresholdMin, thresholdMax;
+            vector<ocl::oclMat> channels;
+            ocl::cvtColor(oclImage, oclImageFilteredCvt, CV_RGB2HSV);
+
+            ocl::split(oclImageFilteredCvt, channels);
+            ocl::threshold(channels[0], thresholdMin, H_min, 255, THRESH_BINARY);
+            ocl::threshold(channels[0], thresholdMax, H_max, 255, THRESH_BINARY_INV);
+            ocl::bitwise_and(thresholdMin, thresholdMax, oclImageFiltered); //imageTmp = oclImageFiltered;imshow(WINDOW_TEST, imageTmp);
             
             //inRange(oclImageFiltered, Scalar(H_min, V_min, S_min), Scalar(H_max, V_max, S_max), oclImageFiltered);
-            imageFiltered = oclImageFiltered;
+            //imageFiltered = oclImageFiltered;
             //inRange seems not to work with ocl ...
-            inRange(imageFiltered, Scalar(H_min, V_min, S_min), Scalar(H_max, V_max, S_max), imageFiltered);
+            //inRange(imageFiltered, Scalar(H_min, V_min, S_min), Scalar(H_max, V_max, S_max), imageFiltered);
         #else
             cvtColor(image, imageFiltered, CV_RGB2HSV);
 
             // Filtering part : Application of the threshold to keep only the color of the ball
             inRange(imageFiltered, Scalar(H_min, V_min, S_min), Scalar(H_max, V_max, S_max), imageFiltered);
         #endif 
-        endFiltering = (float) clock() ;
+        //endFiltering = (float) clock() ;
+        gettimeofday(&tv_end, NULL);
+        filteringTime += (double) (tv_end.tv_sec - tv_begin.tv_sec) + ((double) (tv_end.tv_usec - tv_begin.tv_usec)/1000000);
+
 
         #ifdef DISPLAY
+            #ifdef OCL
+                imageFiltered = oclImageFiltered;
+            #endif
             imshow(WINDOW_ORIGIN, image);
             imshow(WINDOW_THRESHOLD, imageFiltered) ;  
         #endif     
 
 
         // Noise cancelation
-        beginNoise = (float) clock() ;
+        //beginNoise = (float) clock() ;
+        gettimeofday(&tv_begin, NULL);
         #ifdef OCL
-            /*gpu::GpuMat gpuImage(imageFiltered), gpuCircles ;
-
-            gpu::erode ( gpuImage, gpuImage, erodeElement );
-            gpu::erode ( gpuImage, gpuImage, erodeElement );
-            gpu::dilate ( gpuImage, gpuImage, dilateElement );
-            gpu::dilate ( gpuImage, gpuImage, dilateElement );*/
-
-            oclImageFiltered = imageFiltered;
+            //oclImageFiltered = imageFiltered;
 
             ocl::erode ( oclImageFiltered, oclImageFiltered, erodeElement );
             ocl::erode ( oclImageFiltered, oclImageFiltered, erodeElement );
@@ -190,7 +212,9 @@ int main( int argc, char** argv )
             dilate ( imageFiltered, imageFiltered, dilateElement );
             erode ( imageFiltered, imageFiltered, erodeElement );
         #endif
-        endNoise = (float) clock() ;
+        //endNoise = (float) clock() ;
+        gettimeofday(&tv_end, NULL);
+        noiseTime += (double) (tv_end.tv_sec - tv_begin.tv_sec) + ((double) (tv_end.tv_usec - tv_begin.tv_usec)/1000000);
         
         #ifdef DISPLAY
             cout << "New image" << endl ;
@@ -199,36 +223,47 @@ int main( int argc, char** argv )
 
 
         // Contour determination
-        beginContour = (float) clock() ;
+        //beginContour = (float) clock() ;
+        gettimeofday(&tv_begin, NULL);
         #ifdef OCLss
-            //source de lenteur ici, et les cercles ne sont pas détectés ...
+            //source de lenteur ici
             ocl::threshold(oclImageFiltered, oclImageFiltered, 100, 255, THRESH_BINARY) ;
             imageFiltered = oclImageFiltered;
-            HoughCircles(imageFiltered, circles, CV_HOUGH_GRADIENT, 1, 100, 128, 1000, 0, 400 ) ;
-            // n'existe pas en ocl
-            //gpu::HoughCircles(gpuImage, gpuCircles, CV_HOUGH_GRADIENT, 2, 100, 128, 100, 0, 400, 10) ;
+            #ifdef HOUGH
+                HoughCircles(imageFiltered, circles, CV_HOUGH_GRADIENT, 1, 100, 128, 1000, 0, 400 ) ;
+            #else
+                findContours( imageFiltered, contours, contoursHierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
-            /*vector<Vec3f>::const_iterator itc = gpuCircles.begin();
 
-           while (itc!=gpuCircles.end()) 
-           {
-                #ifdef DISPLAY
-                    cout << "Circle found : (" << round((*itc)[0]) << ", " << round((*itc)[1])) << " | " << (*itc)[2] << ")" <<endl ;
-                #else
-                    stringOutput << "Circle found : (" << round((*itc)[0]) << ", " << round((*itc)[1])) << " | " << (*itc)[2] << ")" <<endl ;
-                #endif
-                 
-                ++itc;
-           }*/
-            for( size_t iCircle = 0; iCircle < circles.size(); iCircle++ )
-            {
+                // Find the convex hull object for each contour
+                vector<vector<Point> >hull( contours.size() );
+                for( int i = 0; i < contours.size(); i++ )
+                {  
+                    convexHull( Mat(contours[i]), hull[i], false ); 
 
-                #ifdef DISPLAY
-                    cout << "Circle found : (" << round(circles[iCircle][0]) << ", " << round(circles[iCircle][1]) << " | " << circles[iCircle][2] << ")" <<endl ;
-                #else
-                    stringOutput << "Circle found : (" << round(circles[iCircle][0]) << ", " << round(circles[iCircle][1]) << " | " << circles[iCircle][2] << ")" <<endl ;
-                #endif
-           }
+                    #ifdef DISPLAY
+                        // Draw contours + hull results
+                        drawContours( imageFiltered, contours, i, Scalar(255,255,255), 1, 8, vector<Vec4i>(), 0, Point() );
+                        drawContours( imageFiltered, hull, i, Scalar(255,255,255), 1, 8, vector<Vec4i>(), 0, Point() );
+                    #endif
+                }
+
+
+
+                for ( iteratorContours = contours.begin() ; iteratorContours != contours.end() ; iteratorContours++)
+                {   
+                    minEnclosingCircle ( *iteratorContours, center, radius ) ;
+
+                    #ifdef DISPLAY
+                        cout << "Circle found : (" << round(center.x) << ", " << round(center.y) << " | " << radius << ")" <<endl ;
+                        Point centerCircle(cvRound(center.x), cvRound(center.y)) ;
+                        circle( imageFiltered, centerCircle, cvRound(radius), Scalar(255,255,255), 2, 8, 0 );
+                    #else
+                        stringOutput << "Circle found : (" << round(center.x) << ", " << round(center.y) << " | " << radius << ")" <<endl ;
+                    #endif
+                } 
+
+            #endif
 
         #else
             
@@ -279,7 +314,9 @@ int main( int argc, char** argv )
                 } 
             #endif
         #endif
-        endContour = (float) clock() ;
+        //endContour = (float) clock() ;
+        gettimeofday(&tv_end, NULL);
+        contourTime += (double) (tv_end.tv_sec - tv_begin.tv_sec) + ((double) (tv_end.tv_usec - tv_begin.tv_usec)/1000000);
 
 
         #ifdef DISPLAY
@@ -287,7 +324,7 @@ int main( int argc, char** argv )
         #endif 
 
 
-        end = (float) clock() ;
+        /*end = (float) clock() ;
         calculationTime += end - begin ;
         filteringTime += endFiltering - beginFiltering ;
         noiseTime += endNoise - beginNoise ;
@@ -296,7 +333,7 @@ int main( int argc, char** argv )
         if ( (end - begin) > worst )
             worst = end - begin ;
         else if ( (end - begin)  < best )
-            best = end - begin ;
+            best = end - begin ;*/
 
 
 
@@ -307,18 +344,24 @@ int main( int argc, char** argv )
         }
             
     }
-    size_t endTime = time(NULL);
+    gettimeofday(&endTime, NULL);
+    totalTime = (double) (endTime.tv_sec - beginTime.tv_sec) + ((double) (endTime.tv_usec - beginTime.tv_usec)/1000000);
     
 
     #ifndef DISPLAY
-        cout << stringOutput.str() ;
+        //cout << stringOutput.str() ;
     #endif
 
-    cout << "Total Time : " << endTime - beginTime << "s" << endl;
-    cout << "Filtering executed in " << filteringTime / ((float) i) / ((float) CLOCKS_PER_SEC) << "s on average per frame (frames : " << i << ")" << endl ;
+    cout << "Total Time : " << totalTime << "s" << endl;
+    cout << " Read time : " << readTime << "s" << endl;
+    cout << " Filtering time : " << filteringTime << "s" << endl;
+    cout << " Noise cancellation time : " << noiseTime << "s" << endl;
+    cout << " Contour determination time : " << contourTime << "s" << endl;
+
+    /*cout << "Filtering executed in " << filteringTime / ((float) i) / ((float) CLOCKS_PER_SEC) << "s on average per frame (frames : " << i << ")" << endl ;
     cout << "Noise cancelation executed in " << noiseTime / ((float) i) / ((float) CLOCKS_PER_SEC) << "s on average per frame (frames : " << i << ")" << endl ;
     cout << "Contour determination executed in " << contourTime / ((float) i) / ((float) CLOCKS_PER_SEC) << "s on average per frame (frames : " << i << ")" << endl ;
-    cout << "Code executed in " << calculationTime / ((float) i) / ((float) CLOCKS_PER_SEC) << "s on average per frame (frames : " << i << ",best : " << best / ((float) CLOCKS_PER_SEC) << "s, worst : " << worst / ((float) CLOCKS_PER_SEC) << "s)" << endl ;
+    cout << "Code executed in " << calculationTime / ((float) i) / ((float) CLOCKS_PER_SEC) << "s on average per frame (frames : " << i << ",best : " << best / ((float) CLOCKS_PER_SEC) << "s, worst : " << worst / ((float) CLOCKS_PER_SEC) << "s)" << endl ;*/
 
     return 0;
 }
